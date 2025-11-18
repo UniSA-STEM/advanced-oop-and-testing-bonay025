cc57a9e742aeccacaf9c9f316e3bf8942e214d5c
bonay025 <bonay025@mymail.unisa.edu.au>
2025-11-19 09:17:50 +1100

    Created Staff class, defined init, add responsibility and string conversion methods.

--- staff.py
+++ staff.py
@@ -1,8 +1,23 @@
-'''
+"""
 File: filename.py
 Description: A brief description of this Python module.
 Author: Billy Bizilis
 ID: 110100110
 Username: bizvy001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+"""
+
+class Staff:
+    def __init__(self, staff_name, role):
+        self.__staff_name = staff_name
+        self.__role = role
+        self.__responsibilities = []
+
+    def add_responsibility(self, responsibility):
+        self.__responsibilities.append(responsibility)
+
+    def __str__(self):
+        return (f'Name: {self.__staff_name}\n'
+                f'Role: {self.__role}\n'
+                f'Current responsibilities: {self.__responsibilities}\n')
+
