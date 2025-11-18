b5c547618dda462d80d855fc09610c3330bd956b
bonay025 <bonay025@mymail.unisa.edu.au>
2025-11-19 09:53:06 +1100

    Added getters for first and last name and role.

--- staff.py
+++ staff.py
@@ -1,23 +1,34 @@
 """
-File: filename.py
-Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+File: enclosure.py
+Description: This module hold the Enclosure class which represents a
+zoo enclosure.
+Author: Amelia Bond
+ID: 110457542
+Username: bonay025
 This is my own work as defined by the University's Academic Integrity Policy.
 """
 
 class Staff:
-    def __init__(self, staff_name, role):
-        self.__staff_name = staff_name
+    def __init__(self, first_name, last_name, role):
+        self.__first_name = first_name
+        self.__last_name = last_name
         self.__role = role
-        self.__responsibilities = []
+        self.__duties = []
 
-    def add_responsibility(self, responsibility):
-        self.__responsibilities.append(responsibility)
+    def get_first_name(self):
+        return self.__first_name
+
+    def get_last_name(self):
+        return self.__last_name
+
+    def get_role(self):
+        return self.__role
+
+    def add_duty(self, duty):
+        self.__duties.append(duty)
 
     def __str__(self):
-        return (f'Name: {self.__staff_name}\n'
+        return (f'Name: {self.__first_name} {self.__last_name}\n'
                 f'Role: {self.__role}\n'
-                f'Current responsibilities: {self.__responsibilities}\n')
+                f'Current responsibilities: {self.__duties}\n')
 
