e3530c3f1d0d237a9933846a92b0bd133346c651
bonay025 <bonay025@mymail.unisa.edu.au>
2025-11-19 09:47:59 +1100

    Added methods to set enclosure and animals for the Zookeeper class.

--- zookeeper.py
+++ zookeeper.py
@@ -7,20 +7,31 @@ ID: 110457542
 Username: bonay025
 This is my own work as defined by the University's Academic Integrity Policy.
 """
-
+import enclosure
+from mammal import Mammal
 from staff import Staff
+from enclosure import Enclosure
 
 class Zookeeper(Staff):
     def __init__(self, first_name, last_name, role="Zookeeper"):
         Staff.__init__(self, first_name, last_name, role)
         self.__duties = []
-        self.__enclosures = []
+        self.__enclosure = None
+        self.__animals = []
 
     def add_duty(self, duty):
         Staff.add_duty(self, duty)
 
-    def add_enclosure(self, enclosure):
-        self.__enclosures.append(enclosure)
+    def set_enclosure(self, enclosure):
+        self.__enclosure = enclosure
+        self.add_animal()
+
+    def add_animal(self):
+        list_animals = self.__enclosure.get_animals()
+        for animal in list_animals:
+            self.__animals.append(animal)
+
+
 
 
 
