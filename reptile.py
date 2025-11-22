50f1da14d4f769df7fbd44373d0aeb897adec31f
bonay025 <bonay025@mymail.unisa.edu.au>
2025-11-23 10:33:17 +1100

    Created a file to hold the reptile class and added header comment

---
+++ reptile.py
@@ -0,0 +1,9 @@
+"""
+File: enclosure.py
+Description: This module hold the Enclosure class which represents a
+zoo enclosure.
+Author: Amelia Bond
+ID: 110457542
+Username: bonay025
+This is my own work as defined by the University's Academic Integrity Policy.
+"""
