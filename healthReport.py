44c83e0760bc3a4db04912da2e322b2bbf327b42
bonay025 <bonay025@mymail.unisa.edu.au>
2025-11-23 19:25:56 +1100

    Updated method to generate individual animal reports.

--- healthReport.py
+++ healthReport.py
@@ -6,7 +6,9 @@ ID: 110457542
 Username: bonay025
 This is my own work as defined by the University's Academic Integrity Policy.
 """
-
+from mammal import Mammal
+from veterinarian import Veterinarian
+from healthRecord import HealthRecord
 
 class HealthReport:
     def __init__(self):
@@ -15,16 +17,17 @@ class HealthReport:
     def generate_animal_health_report(self, animal):
         list_records = animal.get_health_record()
         if not list_records:
-            print(f"Health Report for {animal.get_name()}\n"
+            print(f"**{animal.get_name()}**\n"
                   f"No current health information available for {animal.get_name()}\n")
         else:
             str_records = ""
             for record in list_records:
-                str_records += f"{record} \n"
-            print(f"Health Report for {animal.get_name()}\n"
+                str_records += f"{record.get_issue()} {record.get_date()} \n"
+            print(f"**{animal.get_name()}**\n"
                   f"{str_records}")
 
     def generate_zoo_health_report(self, zoo):
         print(f"---Health Report for All Animals---")
         for animal in zoo:
             self.generate_animal_health_report(animal)
+
