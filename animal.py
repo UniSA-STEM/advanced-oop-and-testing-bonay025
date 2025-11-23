5af6c8fd7349485fad88a8cbfdc6191e180ac735
bonay025 <bonay025@mymail.unisa.edu.au>
2025-11-23 15:07:20 +1100

    Updated the eat and string conversion method and added getters for health record, health status, species and environment needs.

--- animal.py
+++ animal.py
@@ -9,18 +9,32 @@ This is my own work as defined by the University's Academic Integrity Policy.
 """
 
 class Animal:
-    def __init__(self, name:str, species:str, age:int, diet:str):
+    def __init__(self, name:str, species:str, age:int, diet:str, environment_needs):
         self.__name = name
         self.__species = species
         self.__age = age
         self.__diet = diet
+        self.__environment_needs = environment_needs
         self.__health_record = []
+        self.__health_status = "Healthy"
+
+    def get_health_record(self):
+        return self.__health_record
+
+    def set_health_status(self, status:str):
+        self.__health_status = status
+
+    def get_species(self):
+        return self.__species
+
+    def get_environment_needs(self):
+        return self.__environment_needs
 
     def make_sound(self):
         print("Making sound")
 
-    def eat(self, food):
-        print(f"Eating {food}")
+    def eat(self):
+        print(f"{self.__name} is eating food")
 
     def sleep(self):
         print("Sleeping")
@@ -39,4 +53,5 @@ class Animal:
                 f"Age: {self.__age} \n"
                 f"Species: {self.__species} \n"
                 f"Diet: {self.__diet} \n"
+                f"Status: {self.__health_status} \n"
                 f"Health record: {str_health_record}\n")
