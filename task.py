55c519cc88a8d4bfaa4cd9f4778c28b039fb73c8
bonay025 <bonay025@mymail.unisa.edu.au>
2025-11-24 09:49:48 +1100

    Created Task class and defined init and string conversion method.

--- task.py
+++ task.py
@@ -7,3 +7,14 @@ Username: bonay025
 This is my own work as defined by the University's Academic Integrity Policy.
 """
 
+
+class Task:
+    def __init__(self, name, time, frequency):
+        self.__name = name
+        self.__time = time
+        self.__frequency = frequency
+
+    def __str__(self):
+        return (f"Task: {self.__name} \n"
+                f"Time: {self.__time} \n"
+                f"Frequency: {self.__frequency} \n")
