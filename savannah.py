57b53ba75d2ecf36bf06965af6378b5c3e304623
bonay025 <bonay025@mymail.unisa.edu.au>
2025-11-23 14:29:08 +1100

    Created file for Savannah enclosure class and added header comment.

---
+++ savannah.py
@@ -0,0 +1,9 @@
+"""
+File: enclosure.py
+Description: This module hold the Savannah class which represents a savannah enclosure.
+zoo enclosure.
+Author: Amelia Bond
+ID: 110457542
+Username: bonay025
+This is my own work as defined by the University's Academic Integrity Policy.
+"""
