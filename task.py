853331a461b558f519817a42706a2e648bde9c2e
bonay025 <bonay025@mymail.unisa.edu.au>
2025-11-24 09:48:07 +1100

    Created new file for task class and added header comment.

---
+++ task.py
@@ -0,0 +1,9 @@
+"""
+File: enclosure.py
+Description: This module holds a task class for staff daily tasks.
+Author: Amelia Bond
+ID: 110457542
+Username: bonay025
+This is my own work as defined by the University's Academic Integrity Policy.
+"""
+
