945562b4120e28ce4abcd7cc4f2755a28b75ce90
bonay025 <bonay025@mymail.unisa.edu.au>
2025-11-23 19:20:05 +1100

    Created Health Report class, defined init and method to generate health report for the whole zoo.

--- healthReport.py
+++ healthReport.py
@@ -6,3 +6,13 @@ ID: 110457542
 Username: bonay025
 This is my own work as defined by the University's Academic Integrity Policy.
 """
+
+
+class HealthReport:
+    def __init__(self):
+        self.__health_records = []
+
+    def generate_zoo_health_report(self, zoo):
+        print(f"---Health Report for All Animals---")
+        for animal in zoo:
+            self.generate_animal_health_report(animal)
