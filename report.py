f0101ded0d6b6360b9f9db829a67b85f2e98f96d
bonay025 <bonay025@mymail.unisa.edu.au>
2025-11-24 08:46:40 +1100

    Created method to generate report of all animals in the zoo by species.

--- report.py
+++ report.py
@@ -11,8 +11,9 @@ from veterinarian import Veterinarian
 from healthRecord import HealthRecord
 
 class HealthReport:
-    def __init__(self):
-        self.__health_records = []
+    def __init__(self, zoo_animals=[], zoo_enclosures=[]):
+        self.__zoo_animals = zoo_animals
+        self.__zoo_enclosures = zoo_enclosures
 
     def generate_animal_health_report(self, animal):
         list_records = animal.get_health_record()
@@ -26,10 +27,25 @@ class HealthReport:
                 str_records += f"{record.get_issue()} {record.get_date()} \n"
             print(f"**{animal.get_name()}**\n"
                   f"Status: {animal.get_health_status()}\n"
-                  f"{str_records}")
+                  f"{str_records}\n")
 
-    def generate_zoo_health_report(self, zoo):
+    def generate_zoo_health_report(self):
         print(f"---Health Report for All Animals---")
-        for animal in zoo:
+        for animal in self.__zoo_animals:
             self.generate_animal_health_report(animal)
+
+    def generate_animal_report(self):
+        animal_species = {}
+        for animal in self.__zoo_animals:
+            animal_species[animal.get_species()] = []
+        for animal in self.__zoo_animals:
+            animal_species[animal.get_species()].append(animal)
+        print(f"---Animals in Zoo---")
+        str_animals = ""
+        for species, animals in animal_species.items():
+            str_animals += f"**{species}s** \n"
+            for animal in animals:
+                str_animals += f"{animal}\n"
+        print(str_animals)
+
 
