857ba6bf5c14d4509b58a6fe07e69459a8bf5fea
bonay025 <bonay025@mymail.unisa.edu.au>
2025-11-23 19:20:44 +1100

    Created method to get health report for an individual animal.

--- healthReport.py
+++ healthReport.py
@@ -12,6 +12,18 @@ class HealthReport:
     def __init__(self):
         self.__health_records = []
 
+    def generate_animal_health_report(self, animal):
+        list_records = animal.get_health_record()
+        if not list_records:
+            print(f"Health Report for {animal.get_name()}\n"
+                  f"No current health information available for {animal.get_name()}\n")
+        else:
+            str_records = ""
+            for record in list_records:
+                str_records += f"{record} \n"
+            print(f"Health Report for {animal.get_name()}\n"
+                  f"{str_records}")
+
     def generate_zoo_health_report(self, zoo):
         print(f"---Health Report for All Animals---")
         for animal in zoo:
