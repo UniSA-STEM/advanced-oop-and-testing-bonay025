6591dbb0c368960131901066aba9884e25cee5af
bonay025 <bonay025@mymail.unisa.edu.au>
2025-11-23 14:36:43 +1100

    Deleted files as decided separate classes are not necessary.

--- aquatic.py
+++
@@ -1,9 +0,0 @@
-"""
-File: enclosure.py
-Description: This module hold the Aquatic class which represents an aquatic enclosure.
-zoo enclosure.
-Author: Amelia Bond
-ID: 110457542
-Username: bonay025
-This is my own work as defined by the University's Academic Integrity Policy.
-"""
