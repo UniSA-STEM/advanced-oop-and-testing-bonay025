12dd0a579760983aad9720d58a89853808ef48ab
bonay025 <bonay025@mymail.unisa.edu.au>
2025-11-24 18:08:12 +1100

    Added method for bird to fly.

--- bird.py
+++ bird.py
@@ -11,7 +11,13 @@ This is my own work as defined by the University's Academic Integrity Policy.
 from animal import Animal
 
 class Bird(Animal):
-    def __init__(self, name, species, age, diet, environment_needs, wing_span):
+    def __init__(self, name, species, age, diet, environment_needs, flying = True):
         Animal.__init__(self, name, species, age, diet, environment_needs)
-        self._wing_span = wing_span
+        self.__flying = flying
+
+    def fly(self):
+        if self.__flying:
+            print(f"{self.get_name()} is flying.")
+        else:
+            print(f"{self.get_name()} cannot flying.")
 
